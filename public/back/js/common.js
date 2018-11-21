59e6dbeb7c7e6e706bd7a9420a947e5ad6ba57bc
Anniejs <anniejs@163.com>
2018-11-21 18:45:50 +0800

    登录拦截和切换菜单完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -26,8 +26,58 @@ $(document).ajaxStop(function() {
 });
 
 
-//功能2-进行二级分类切换
+// 在一进入页面进行登录状态获取
+// 如果后端响应头中设置了 Content-Type: application/json
+// jquery 会自动识别, 将返回数据类型, 当成json字符串解析成对象
+if ( location.href.indexOf("login.html") === -1 ) {
+  $.ajax({
+    url: "/employee/checkRootLogin",
+    type: "get",
+    success: function( info ) {
+      console.log( info )
+      if ( info.success ) {
+        console.log( "登陆了" );
+        // 啥也不用干
+      }
+
+      if ( info.error === 400 ) {
+        // 进行拦截, 拦截到登录页
+        location.href = "login.html";
+      }
+    }
+  })
+}
+
+
+//功能1-进行二级分类切换
 $(".category").click(function(){
   $(this).next().stop().slideToggle();
 });
 
+
+//2-顶部菜单栏切换显示
+$(".icon_menu").click(function(){
+  
+  $(".lt_aside").toggleClass("hidemenu");
+  $(".lt_main").toggleClass("hidemenu");
+  $(".lt_topbar").toggleClass("hidemenu");
+});
+
+//3-点击图标退出模态框
+
+
+$(".icon_logout").click(function(){
+  $("#logoutModal").modal(show);
+})
+
+$(".logout").click(function(){
+  $.ajax({
+    url:"/employee/employeeLogout",
+    type:"get",
+    dataType:"json",
+    success:function(info){
+      //判断如果成功就直接跳转到登录页面
+      location.href = "login.html";
+    }
+  })
+})
